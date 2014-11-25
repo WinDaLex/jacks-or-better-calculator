41461e7457dd05be8daf714e766dbaf4470ad797
WinDaLex <windalex6@gmail.com>
2014-11-26 00:36:01 +0800

    Update unit testing of the Judge class

--- TestJudgeClass.py
+++ TestJudgeClass.py
@@ -6,9 +6,42 @@ from Judge import *
 class TestJudgeClass(unittest.TestCase):
 
     def testRoyalFlush(self):
-        self.judge = Judge([Card('club', '10'), Card('club', 'J'), Card('club', 'Q'), Card('club', 'K'), Card('club', 'A')])
-        self.assertEqual(self.judge.result, 'Royal Flush')
-        self.assertEqual(self.judge.payoff, 5000)
+        for suit in Card.suits:
+            judge = Judge([Card(suit, 'A'), Card(suit, '10'), \
+                    Card(suit, 'J'), Card(suit, 'K'), Card(suit, 'Q')])
+            self.assertEqual([judge.result, judge.payoff], ['Royal Flush', 5000])
+
+    def testStraightFlush(self):
+        judge = Judge([Card('club', '5'), Card('heart', '3'), \
+                Card('diamand', '6'), Card('diamand', '4'), Card('heart', '7')])
+        self.assertEqual([judge.result, judge.payoff], ['Straight Flush', 1500])
+
+    def testFourOfAKind(self):
+        judge = Judge([Card('spade', '9'), Card('heart', '9'), \
+                Card('diamand', '9'), Card('heart', '10'), Card('club', '9')])
+        self.assertEqual([judge.result, judge.payoff], ['Four of a Kind', 600])
+
+    def testFullHouse(self):
+        pass
+
+    def testFlush(self):
+        pass
+
+    def testStraight(self):
+        pass
+
+    def testThreeOfAKind(self):
+        pass
+
+    def testTwoPair(self):
+        pass
+
+    def testJacksOrBetter(self):
+        pass
+
+    def testNothing(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
