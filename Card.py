0d1884d0b72578c4e52c88cd163c070add1a50a3
WinDaLex <windalex6@gmail.com>
2014-11-26 20:18:56 +0800

    Add Hand class and improve Card class by using Exception

--- Card.py
+++ Card.py
@@ -5,10 +5,48 @@ class Card():
     ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
     def __init__(self, suit, rank):
-        if suit not in self.suits: print 'SUIT ERROR'; return
-        if rank not in self.ranks: print 'RANK ERROR'; return
+        if suit not in self.suits: raise ValueError, 'wrong suit'; return
+        if rank not in self.ranks: raise ValueError, 'wrong rank'; return 
         self.suit = suit
         self.rank = rank
 
-    def show(self):
-        print self.suit, self.rank
+    def __str__(self):
+        return self.suit + ' ' + self.rank
+
+
+class Hand():
+    """A hand is five distinct cards which players got."""
+
+    def __init__(self, cards):
+        """ cards can be 'suit rank, suit rank, suit rank, suit rank, suit rank' """ 
+        self.cards = cards
+        if type(cards) == type(''):
+            self.cards = []
+            for suit_and_rank in cards.split(','):
+                suit, rank = suit_and_rank.strip().split(' ')
+                self.cards.append(Card(suit, rank))
+        if type(self.cards) == type([]):
+            if len(self.cards) != 5:
+                raise ValueError, 'cards should contain five cards, it is only %d' % len(self.cards)
+            for card in self.cards:
+                if card.__class__.__name__ != 'Card':
+                    raise ValueError, 'cards can only contain Card, %s is not allowed' % type(card)
+        else:
+            raise ValueError, 'cards should be str or list, cannot be %s' % type(cards)
+
+    def __getitem__(self, ind):
+        return self.cards.__getitem__(ind)
+
+    def dividedBySuit(self):
+        return self.__dividedBy('suit')
+
+    def dividedByRank(self):
+        return self.__dividedBy('rank')
+
+    def __dividedBy(self, attr):
+        res = {}
+        for card in self.cards:
+            attr_val = getattr(card, attr)
+            if attr_val not in res: res[attr_val] = 0
+            res[attr_val] += 1
+        return res
