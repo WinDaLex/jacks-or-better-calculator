e823ff4c599d80ee325aa74f78be9e39c49cef6f
WinDaLex <windalex6@gmail.com>
2014-12-28 17:12:22 +0000

    Transform Judge into a module rather than a class

--- Judge.py
+++ Judge.py
@@ -1,70 +1,62 @@
-class Judge():
+from Card import *
 
-    PAYOFF = {'Royal Flush': 5000, 'Straight Flush': 1500, \
-            'Four of a Kind': 600, 'Full House': 300, 'Flush': 200, \
-            'Straight': 125, 'Three of a Kind': 75, 'Two Pair': 40, \
-            'Jacks or Better': 10, 'Nothing': 0}
+PAYOFF = {'Royal Flush': 5000, 'Straight Flush': 1500, \
+        'Four of a Kind': 600, 'Full House': 300, 'Flush': 200, \
+        'Straight': 125, 'Three of a Kind': 75, 'Two Pair': 40, \
+        'Jacks or Better': 10, 'Nothing': 0}
 
-    def __init__(self, hand):
-        self.hand = hand
-        self.result = self.compute()
-        self.payoff = self.PAYOFF[self.result]
+def judge(hand):
+    result = compute(hand)
+    return result, PAYOFF[result]
 
-    def compute(self):
-        m = self.hand.dividedByRank()
-        numOfRank = sorted(m.values(), reverse=True)
+def compute(hand):
+    m = hand.dividedByRank()
+    numOfRank = sorted(m.values(), reverse=True)
 
-        if numOfRank[0] == 4: return 'Four of a Kind'
-        if numOfRank[0] == 3 and numOfRank[1] == 2: return 'Full House'
-        if numOfRank[0] == 3: return 'Three of a Kind'
-        if numOfRank[0] == 2 and numOfRank[1] == 2: return 'Two Pair'
-        if numOfRank[0] == 2:
+    if numOfRank[0] == 4: return 'Four of a Kind'
+
+    if numOfRank[0] == 3:
+        if numOfRank[1] == 2: return 'Full House'
+        else: return 'Three of a Kind'
+
+    if numOfRank[0] == 2:
+        if numOfRank[1] == 2: return 'Two Pair'
+    else:
+        for i in range(0, 5):
+            if hand[i].rank in ['J', 'Q', 'K', 'A']:
+                for j in range(i+1, 5):
+                    if hand[i].rank == hand[j].rank:
+                        return 'Jacks or Better'
+
+    if isFlush(hand):
+        if isStraight(hand):
             for i in range(0, 5):
-                if self.hand[i].rank in ['J', 'Q', 'K', 'A']:
-                    for j in range(i+1, 5):
-                        if self.hand[i].rank == self.hand[j].rank:
-                            return 'Jacks or Better'
-
-        '''
-        if self.isFlush() and self.isStraight():
-            for i in range(0, 5):
-                if self.hand[i].rank == 'A':
+                if hand[i].rank == 'A':
                     return 'Royal Flush'
             return 'Straight Flush'
+        else:
+            return 'Flush'
 
-        if self.isFlush(): return 'Flush'
-        if self.isStraight(): return 'Straight'
-        '''
+    if isStraight(hand): return 'Straight'
 
-        if self.isFlush():
-            if self.isStraight():
-                for i in range(0, 5):
-                    if self.hand[i].rank == 'A':
-                        return 'Royal Flush'
-                return 'Straight Flush'
-            else:
-                return 'Flush'
+    return 'Nothing'
 
-        if self.isStraight(): return 'Straight'
+def isFlush(hand):
+    return hand.dividedBySuit().values()[0] == 5
 
-        return 'Nothing'
+def isStraight(hand):
+    l = []
+    for i in range(0, 5):
+        if hand[i].rank == 'A': l.extend([1, 14])
+        elif hand[i].rank == '10': l.append(10)
+        elif hand[i].rank == 'J': l.append(11)
+        elif hand[i].rank == 'Q': l.append(12)
+        elif hand[i].rank == 'K': l.append(13)
+        else: l.append(int(hand[i].rank))
+    l.sort()
+    bo = True
+    for i in range(2, 5):
+        if l[i] != l[i - 1] + 1:
+            bo = False
 
-    def isFlush(self):
-        return self.hand.dividedBySuit().values()[0] == 5
-
-    def isStraight(self):
-        l = []
-        for i in range(0, 5):
-            if self.hand[i].rank == 'A': l.extend([1, 14])
-            elif self.hand[i].rank == '10': l.append(10)
-            elif self.hand[i].rank == 'J': l.append(11)
-            elif self.hand[i].rank == 'Q': l.append(12)
-            elif self.hand[i].rank == 'K': l.append(13)
-            else: l.append(int(self.hand[i].rank))
-        l.sort()
-        bo = True
-        for i in range(2, 5):
-            if l[i] != l[i - 1] + 1:
-                bo = False
-
-        if bo: return l[0] == l[1] - 1 or len(l) == 6 and l[5] == 14 and l[4] == 13
+    if bo: return l[0] == l[1] - 1 or len(l) == 6 and l[5] == 14 and l[4] == 13
--- TestJudgeClass.py
+++ TestJudgeClass.py
@@ -1,59 +1,59 @@
 import unittest
 
 from Card import *
-from Judge import *
+import Judge
 
 class TestJudgeClass(unittest.TestCase):
 
     def testRoyalFlush(self):
         for suit in Card.suits:
-            judge = Judge(Hand([Card(suit, 'A'), Card(suit, '10'), \
+            result, payoff = Judge.judge(Hand([Card(suit, 'A'), Card(suit, '10'), \
                     Card(suit, 'J'), Card(suit, 'K'), Card(suit, 'Q')]))
-            self.assertEqual([judge.result, judge.payoff], ['Royal Flush', 5000])
+            self.assertEqual([result, payoff], ['Royal Flush', 5000])
 
     def testStraightFlush(self):
-        judge = Judge(Hand('club 5, club 3, club 6, club 4, club 7'))
-        self.assertEqual([judge.result, judge.payoff], ['Straight Flush', 1500])
+        result, payoff = Judge.judge(Hand('club 5, club 3, club 6, club 4, club 7'))
+        self.assertEqual([result, payoff], ['Straight Flush', 1500])
 
     def testFourOfAKind(self):
-        judge = Judge(Hand('spade 9, heart 9, diamand 9, heart 10, club 9'))
-        self.assertEqual([judge.result, judge.payoff], ['Four of a Kind', 600])
+        result, payoff = Judge.judge(Hand('spade 9, heart 9, diamand 9, heart 10, club 9'))
+        self.assertEqual([result, payoff], ['Four of a Kind', 600])
 
     def testFullHouse(self):
-        judge = Judge(Hand('spade A, heart A, diamand A, club 2, spade 2'))
-        self.assertEqual([judge.result, judge.payoff], ['Full House', 300])
+        result, payoff = Judge.judge(Hand('spade A, heart A, diamand A, club 2, spade 2'))
+        self.assertEqual([result, payoff], ['Full House', 300])
 
     def testFlush(self):
-        judge = Judge(Hand('spade A, spade K, spade 5, spade 4, spade J'))
-        self.assertEqual([judge.result, judge.payoff], ['Flush', 200])
+        result, payoff = Judge.judge(Hand('spade A, spade K, spade 5, spade 4, spade J'))
+        self.assertEqual([result, payoff], ['Flush', 200])
 
     def testStraight(self):
-        judge = Judge(Hand('spade A, heart 2, diamand 3, heart 4, spade 5'))
-        self.assertEqual([judge.result, judge.payoff], ['Straight', 125])
-        judge = Judge(Hand('spade 10, heart J, diamand Q, heart K, spade A'))
-        self.assertEqual([judge.result, judge.payoff], ['Straight', 125])
+        result, payoff = Judge.judge(Hand('spade A, heart 2, diamand 3, heart 4, spade 5'))
+        self.assertEqual([result, payoff], ['Straight', 125])
+        result, payoff = Judge.judge(Hand('spade 10, heart J, diamand Q, heart K, spade A'))
+        self.assertEqual([result, payoff], ['Straight', 125])
 
     def testThreeOfAKind(self):
-        judge = Judge(Hand('spade A, heart A, diamand A, heart 4, spade 5'))
-        self.assertEqual([judge.result, judge.payoff], ['Three of a Kind', 75])
+        result, payoff = Judge.judge(Hand('spade A, heart A, diamand A, heart 4, spade 5'))
+        self.assertEqual([result, payoff], ['Three of a Kind', 75])
 
     def testTwoPair(self):
-        judge = Judge(Hand('spade A, heart A, diamand 4, heart 4, spade 5'))
-        self.assertEqual([judge.result, judge.payoff], ['Two Pair', 40])
+        result, payoff = Judge.judge(Hand('spade A, heart A, diamand 4, heart 4, spade 5'))
+        self.assertEqual([result, payoff], ['Two Pair', 40])
 
     def testJacksOrBetter(self):
-        judge = Judge(Hand('spade J, heart J, diamand 4, heart 9, spade 5'))
-        self.assertEqual([judge.result, judge.payoff], ['Jacks or Better', 10])
+        result, payoff = Judge.judge(Hand('spade J, heart J, diamand 4, heart 9, spade 5'))
+        self.assertEqual([result, payoff], ['Jacks or Better', 10])
 
     def testNothing(self):
-        judge = Judge(Hand('club A, club 3, heart 6, club 4, club 7'))
-        self.assertEqual([judge.result, judge.payoff], ['Nothing', 0])
-        judge = Judge(Hand('spade A, club K, spade 5, spade 4, spade J'))
-        self.assertEqual([judge.result, judge.payoff], ['Nothing', 0])
-        judge = Judge(Hand('spade 6, heart A, diamand 4, heart 4, spade 5'))
-        self.assertEqual([judge.result, judge.payoff], ['Nothing', 0])
-        judge = Judge(Hand('spade 2, heart 4, diamand 5, heart 6, spade 7'))
-        self.assertEqual([judge.result, judge.payoff], ['Nothing', 0])
+        result, payoff = Judge.judge(Hand('club A, club 3, heart 6, club 4, club 7'))
+        self.assertEqual([result, payoff], ['Nothing', 0])
+        result, payoff = Judge.judge(Hand('spade A, club K, spade 5, spade 4, spade J'))
+        self.assertEqual([result, payoff], ['Nothing', 0])
+        result, payoff = Judge.judge(Hand('spade 6, heart A, diamand 4, heart 4, spade 5'))
+        self.assertEqual([result, payoff], ['Nothing', 0])
+        result, payoff = Judge.judge(Hand('spade 2, heart 4, diamand 5, heart 6, spade 7'))
+        self.assertEqual([result, payoff], ['Nothing', 0])
 
 
 if __name__ == '__main__':
